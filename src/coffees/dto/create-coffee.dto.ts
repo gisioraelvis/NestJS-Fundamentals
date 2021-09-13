8ed682de4945d5715ec36e50a5812c16f00f18e5
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-09-13 15:23:26 +0300

    Swagger description with @ApiProperty

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,10 +1,14 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'The name of the coffee' })
   @IsString()
   readonly name: string;
+  @ApiProperty({ description: 'The brand of the coffee' })
   @IsString()
   readonly brand: string;
+  @ApiProperty({ description: 'The coffee flavors' })
   @IsString({ each: true })
   readonly flavors: string[];
 }
