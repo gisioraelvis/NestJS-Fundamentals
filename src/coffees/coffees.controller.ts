c87a12c0a775e94c6aeda6fa272f7ba7e596558d
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-09-13 11:54:38 +0300

    Service providers with dependency injection

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,32 +8,35 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-
-    return `This action returns all coffees with limit:${limit} and offset:${offset}`;
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
+
   @Get(':id')
-  findById(@Param('id') params: string): string {
-    return `This action returns a #${params} coffee`;
+  findById(@Param('id') id: string) {
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
-  create(@Body() body): string {
-    return body;
+  create(@Body() body) {
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body('name') body): string {
-    return `This action updates a #${id} coffee with ${body}`;
+  update(@Param('id') id: string, @Body('name') body) {
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): string {
-    return `This action removes a #${id} coffee`;
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
   }
 }
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,51 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Cappuccino',
+      brand: 'Superstar',
+      flavors: ['vanilla', 'chocolate'],
+    },
+  ];
+
+  findAll(): Coffee[] {
+    return this.coffees;
+  }
+
+  findOne(id: string): Coffee {
+    const coffee = this.coffees.find((coffee) => coffee.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee with id: ${id} notfound`);
+    }
+    return coffee;
+  }
+
+  create(coffee: any) {
+    this.coffees.push(coffee);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      existingCoffee.name = updateCoffeeDto.name;
+      existingCoffee.brand = updateCoffeeDto.brand;
+      existingCoffee.flavors = updateCoffeeDto.flavors;
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((coffee) => coffee.id === +id);
+    if (coffeeIndex !== -1) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
