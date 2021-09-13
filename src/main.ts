25e4e3c283e124eed1f19e9776b53f3354d4b162
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-09-13 14:50:44 +0300

    OpenAPI Specs docs with swagger-ui-express

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS ')
+    .setDescription('NestJS Course Fundamentals API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
