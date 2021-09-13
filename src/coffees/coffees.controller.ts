add6be790ec82b082c5cc9a3a2b91eda77c635f9
Gisiora Elvis <gisioraelvis20@gmail.com>
2021-09-13 15:34:10 +0300

    Swagger tag with @ApiTag

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,11 +8,12 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-import { ApiNotFoundResponse, ApiResponse } from '@nestjs/swagger';
+import { ApiNotFoundResponse, ApiTags } from '@nestjs/swagger';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
